6adea36e0de6e73b6db88e3de666731b4fb331ee
Bhargavbaluni <balunibhargav@gmail.com>
2026-07-12 19:10:32 +0530

    Fix getUnassignedTeams

--- Route/Team.route.js
+++ Route/Team.route.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const {
-  updateTeamRanking,
-  getAllTeams,
-  getPlayerTeams,
-} = require("../Controllers/Team.controller");
+
+const controller = require("../Controllers/Team.controller");
+
+console.log("Controller:", controller);
+
 const { isAdmin } = require("../MiddleWare/authMiddleware");
 
 const router = express.Router();
 
-router.get("/all", getAllTeams);
-router.get("/unassigned", getUnassignedTeams);
-router.get("/:id", getPlayerTeams);
-router.put("/update-ranking", isAdmin, updateTeamRanking);
+router.get("/all", controller.getAllTeams);
+router.get("/unassigned", controller.getUnassignedTeams);
+router.get("/:id", controller.getPlayerTeams);
+router.put("/update-ranking", isAdmin, controller.updateTeamRanking);
 
 module.exports = router;
